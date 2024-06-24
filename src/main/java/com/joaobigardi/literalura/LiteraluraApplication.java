2bf058d710f00776aa14c25be7fc92b1547b2c52
joaobigardi <joaovictor-bigardi@hotmail.com>
2024-06-23 22:34:45 -0300

    Projeto final completo

--- src/main/java/com/joaobigardi/literalura/LiteraluraApplication.java
+++ src/main/java/com/joaobigardi/literalura/LiteraluraApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiteraluraApplication implements CommandLineRunner{
+public class LiteraluraApplication implements CommandLineRunner {
 	@Autowired
 	private BookRepository bookRepository;
 
